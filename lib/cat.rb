cba76a4be6a0dde3e2f3051a47f84864a9fc7be5
Denise Pendleton <dpendl1@hotmail.com>
2018-03-06 19:49:39 +0000

    automatically backed up by learn

--- lib/cat.rb
+++ lib/cat.rb
@@ -1,7 +1,7 @@
 class Cat
 
   attr_accessor :mood, :owner
-    attr_reader :name
+    attr_reader :name, :species
     def initialize(name, mood = "nervous")
       @name = name
       @mood = mood
