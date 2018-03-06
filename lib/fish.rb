5e76b91574db6d077318cd3be7fcf8a408028f03
Denise Pendleton <dpendl1@hotmail.com>
2018-03-06 19:50:19 +0000

    automatically backed up by learn

--- lib/fish.rb
+++ lib/fish.rb
@@ -2,9 +2,10 @@ class Fish
   attr_accessor :mood, :owner
   attr_reader :name, :species
 
-  def initialize(name, mood = "nervous")
+  def initialize(name, mood = "nervous", species)
     @name = name
     @mood = mood
+    @species = species
 
   end
 end
