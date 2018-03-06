ba624f5625b47fa9f51ae1182e69674d15f18f9e
Denise Pendleton <dpendl1@hotmail.com>
2018-03-06 23:21:59 +0000

    automatically backed up by learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -67,7 +67,7 @@ def list_pets
   #   cat_count = cat_arr.length
   #   fish_count = fish.arr.length
   # "I have 2 fish, 3 dog(s), and 1 cat(s)."
-  @pets.each_value.collect {|key, value| "I have #{value.length} fish, #{value.length} dog(s), and #{value.length} cat(s)."}
+  @pets.each_value.collect {|value| "I have #{value.length} fish, #{value.length} dog(s), and #{value.length} cat(s)."}
 end
 
 def self.all
